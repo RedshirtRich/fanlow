308b8ef67ae2c869f33d5760c582e657ebe8b711
Rich Tanner <rwtanner@iastate.edu>
2014-09-23 23:41:17 -0500

    switch to swift, use collectionViews

---
+++ Fanlow/Fanlow/FanlowOfferCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  FanlowOfferCollectionViewCell.swift
+//  Fanlow
+//
+//  Created by Rich Tanner on 9/23/14.
+//  Copyright (c) 2014 Fanlow. All rights reserved.
+//
+
+import UIKit
+
+class FanlowOfferCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var dateLabel: UILabel?
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
---
+++ Fanlow/Fanlow/MasterViewController.swift
@@ -0,0 +1,81 @@
+//
+//  MasterViewController.swift
+//  Fanlow
+//
+//  Created by Rich Tanner on 9/23/14.
+//  Copyright (c) 2014 Fanlow. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UICollectionViewController {
+
+    var objects = NSMutableArray()
+    var selectedIndex: Int = -1
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var owlHeader = UIImage(named: "owlHeader")
+        navigationItem.titleView = UIImageView(image: owlHeader)
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func insertNewObject(sender: AnyObject) {
+        objects.insertObject(NSDate.date(), atIndex: 0)
+        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+        self.collectionView?.insertItemsAtIndexPaths([indexPath])
+    }
+
+    // MARK: - Segues
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showOfferDetail" {
+            if selectedIndex != -1 {
+                let object = objects[selectedIndex] as NSDate
+                (segue.destinationViewController as DetailViewController).detailItem = object
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return objects.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as FanlowOfferCollectionViewCell
+            
+        let object = objects[indexPath.row] as NSDate
+        cell.dateLabel?.text = object.description
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        selectedIndex = indexPath.row
+    }
+
+}
+
